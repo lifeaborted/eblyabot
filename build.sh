bdb92d425304440695458c083720dc4f603e6ca0
lifeaborted <yaz4ebalsya@yandex.ru>
2026-07-28 18:41:48 +0500

    P I Z D E C

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 set -o errexit
 
-echo "Запуск Cloudflare WARP SOCKS5..."
-# Запускаем wireproxy в фоне
-wireproxy -c warp.conf &
+# Ищем конфиг либо в секретах Render, либо в текущей папке
+CONFIG_PATH="warp.conf"
+if [ -f "/etc/secrets/warp.conf" ]; then
+    CONFIG_PATH="/etc/secrets/warp.conf"
+fi
 
-# Даем прокси 3 секунды на инициализацию
+echo "Запуск Cloudflare WARP SOCKS5 (Конфиг: $CONFIG_PATH)..."
+wireproxy -c "$CONFIG_PATH" &
+
+# Даем прокси время на подключение
 sleep 3
 
 echo "Запуск Telegram бота..."
